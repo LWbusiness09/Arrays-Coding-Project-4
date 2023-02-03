8f8d3965a138ed67255bdd22f5ca58bc75680e21
lucasw1usf <122409945+lucasw1usf@users.noreply.github.com>
2023-02-02 22:51:18 -0500

    Add files via upload

---
+++ Deliverable(4) Arrays/Deliverable(4) Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+  Author: Lucas Werneck
+  Date: 02/02/2023
+  Comments: This C# Console application code computes the first 25
+            numbers of the Fibonacci sequence, where
+            f (n) = f (n - 2) + f (n - 1)
+*/
+
+using System;
+
+namespace Deliverable_4__Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int[] fibonacci = new int[25];
+
+                // Initialize fibonacci series
+                fibonacci[0] = 0;
+                fibonacci[1] = 1;
+
+                for (int i = 2; i < 25; i++)
+                {
+                   fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+                
+                for (int i = 0; i < 25; i++)
+                {
+                    Console.WriteLine("Fibonnaci number (" + i + ") = " + fibonacci[i]);
+                }        
+        }
+    }
+}
